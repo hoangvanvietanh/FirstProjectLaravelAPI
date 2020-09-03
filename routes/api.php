4d2af31ef55d325eee3b77109d4731ee6d63d7d0
[Lampart] Hoang Van Viet Anh <viet_anh@lampart-vn.com>
2020-09-03 16:04:23 +0700

    okey

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::view('/', 'welcome');
 
 Route::post('register', 'API\RegisterController@register');
 Route::post('login', 'API\RegisterController@login');
