9e11415937924cc005f9fc8983be0711fe04e643
[Lampart] Hoang Van Viet Anh <viet_anh@lampart-vn.com>
2020-09-03 15:58:25 +0700

    okey

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,6 @@ Route::middleware('auth:api')->group( function () {
     Route::post('assignuserrole/{role}', 'API\PermissionController@assign_user_to_role');
 });
 
-Route::fallback(function(){
-    return response()->json(['message' => 'Not Found.'], 404);
-})->name('api.fallback.404');
+//Route::fallback(function(){
+//    return response()->json(['message' => 'Not Found.'], 404);
+//})->name('api.fallback.404');
